698bd212b48857fc7fc89fc00a2f4d5dd87034f5
paigni <108606503+paigni@users.noreply.github.com>
2022-08-09 20:07:29 +0600

    Add files via upload

--- check_module.py
+++ check_module.py
@@ -1,32 +1,14 @@
 def inp_check(name_func):
     """Проверка ввода числового значения"""
     if name_func.isdigit():
-        return name_func
-    while not name_func.isdigit():
-        repeat = input('Неверный ввод,повторите попытку'"\n")
-        if repeat.isdigit():
-            return repeat
+        return True
+    if not name_func.isdigit():
+        return False
 
 
-def check_categories(number_of_category):
+def check_range(min,max,user_input):
     """Проверка выбора категорий, если пользователь ввёл значение не из номера категории, предлагается ввести заново"""
-    while number_of_category > 6 or number_of_category < 1:
-        repeat = input('Вы выбрали пока недоступный номер категории,повторите ввод'"\n")
-        check_repeat = int(inp_check(repeat))
-        if 1 < check_repeat < 7:
-            return check_repeat
-    if 1 <= number_of_category <= 6:
-        return number_of_category
-
-
-def check_players_count(players_count):
-    """Функция проверяет находится ли число в заданном диапазоне, если нет, возвращает ввод"""
-    while players_count > 10 or players_count < 1:
-        repeat = input('Количество игроков не может быть больше 10'"\n")
-        check_repeat = int(inp_check(repeat))
-        if 1 <= check_repeat <= 10:
-            return check_repeat
-        else:
-            continue
-    if 1 <= players_count <= 10:
-        return players_count
+    if user_input > max or min < user_input:
+        return False
+    if min <= user_input <= max:
+        return True
--- inteface_module.py
+++ inteface_module.py
@@ -1,22 +1,37 @@
-from check_module import inp_check, check_categories, check_players_count
+from check_module import inp_check, check_range
 
 
 def category_choice(cat):
     """Функция выводит список категорий и запрашивает ответ от пользователя"""
     for category, val in enumerate(cat.values(), 1):
         print(f'{category}:{val}')
-
+    max_cat = 6
+    min_cat = 1
     change_category = input(f"Выберите категорию из списка выше"'\n')
     check_user_input = inp_check(change_category)
-    change_category_check_to_is_int = int(check_user_input)
-    change_category_check = check_categories(change_category_check_to_is_int)
-    return cat[change_category_check]
+    if check_user_input == True:
+        change_category_check_to_is_int = int(check_user_input)
+        change_category_check = check_range(min_cat,max_cat,change_category_check_to_is_int)
+        return cat[change_category_check]
+    if check_user_input == False:
+        repeat = input("Повтторите ввод")
+        change_category_check_to_is_int = int(repeat)
+        change_category_check = check_range(min_cat,max_cat,change_category_check_to_is_int)
+        return cat[change_category_check]
 
 
 def number_of_players():
     """Функция запрашивает количество игроков, возвращает ввод от пользователя"""
+    min_play = 2
+    max_play = 10
     players_count = input(f"Введите количество игроков"'\n')
-    players_count_check = check_players_count(int(inp_check(players_count)))
+    players_count_check = inp_check(players_count)
+    if players_count_check == True:
+        players_count_check_1 = int(players_count_check)
+        change_category_check = check_range(min_play,max_play,players_count_check_1)
+        return players_count
+
+
     return players_count_check
 
 
@@ -24,7 +39,7 @@ def player_name(number):
     """Принимает количество игроков в качестве аргумента, возвращая список игроков"""
     players_name = []
     for count in range(0, number):
-        players_name.append('player' + f'{count}')
+        players_name.append(f'player{count}')
     return players_name
 
 
